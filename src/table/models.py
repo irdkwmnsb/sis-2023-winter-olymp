7d75cd3bc1fbab6879b462f2b523565d46ef11ac
Andrew Gein <andgein@yandex.ru>
2017-12-31 10:53:38 +0200

    Бла бла бла

--- src/table/models.py
+++ src/table/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from enum import Enum
+
+
+class Gender(Enum):
+    MALE = 0
+    FEMALE = 1
+    NEUTER = 2
 
 
 class Resource(models.Model):
@@ -7,9 +14,23 @@ class Resource(models.Model):
     name_acc_two = models.CharField(max_length=255, help_text='Например, (две) пушки')
     name_acc_five = models.CharField(max_length=255, help_text='Например, (пять) пушек')
     color = models.CharField(max_length=10, help_text='blue, yellow, red или green')
+    gender = models.PositiveIntegerField(
+        choices=[(Gender.MALE.value, 'Male'), (Gender.FEMALE.value, 'Female'), (Gender.NEUTER.value, 'Neuter')]
+    )
 
     def __str__(self):
         return self.name
+
+    def get_name_for_count(self, count):
+        last = count % 10
+        prelast = (count // 10) % 10
+        if prelast != 1 and 2 <= last <= 4:
+            return self.name_acc_two
+        if 5 <= last <= 9 or last == 0:
+            return self.name_acc_five
+        if prelast == 1:
+            return self.name_acc_five
+        return self.name_acc_one
 
 
 class Country(models.Model):
@@ -39,6 +60,52 @@ class Card(models.Model):
 
     def get_gives(self):
         return GivesCardResource.objects.filter(card=self).all()
+
+    def _get_resources_description(self, resources):
+        result = ''
+        for idx, cr in enumerate(resources):
+            result += ('%s %s' % (
+                self._resource_count_to_string(cr.resource, cr.count), cr.resource.get_name_for_count(cr.count)
+            )).strip()
+            if idx < len(resources) - 2:
+                result += ', '
+            elif idx == len(resources) - 2:
+                result += ' и '
+        return result
+
+    def get_needs_description(self):
+        return self._get_resources_description(list(self.get_needs()))
+
+    def get_gives_description(self):
+        return self._get_resources_description(list(self.get_gives()))
+
+    def _resource_count_to_string(self, resource, count):
+        male_ints = {
+            1: '',
+            2: 'два',
+            3: 'три',
+            4: 'четыре',
+            5: 'пять',
+            6: 'шесть',
+            7: 'семь',
+            8: 'восемь',
+            9: 'девять',
+            10: 'десять',
+        }
+        female_ints = {
+            # 1: 'одну',
+            2: 'две',
+        }
+        neuter_ints = {
+            # 1: 'одно',
+        }
+        if resource.gender == Gender.NEUTER.value and count in neuter_ints:
+            return neuter_ints[count]
+        if resource.gender == Gender.FEMALE.value and count in female_ints:
+            return female_ints[count]
+        if count in male_ints:
+            return male_ints[count]
+        return str(count)
 
 
 class VirtualContest(models.Model):
--- src/table/templatetags/helpers.py
+++ src/table/templatetags/helpers.py
@@ -13,15 +13,8 @@ def get_item(dictionary, key):
 
 @register.filter
 def select_resource_name(resource, count):
-    last = count % 10
-    prelast = (count // 10) % 10
-    if prelast != 1 and 2 <= last <= 4:
-        return resource.name_acc_two
-    if 5 <= last <= 9 or last == 0:
-        return resource.name_acc_five
-    if prelast == 1:
-        return resource.name_acc_five
-    return resource.name_acc_one
+    return resource.get_name_for_count(count)
+
 
 @register.filter
 def times(number):
